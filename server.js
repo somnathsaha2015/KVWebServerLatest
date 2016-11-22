bcc14998c9f09a16e4458536a4af63eb13eb8022
somnathsaha2015 <somnathsaha2005@gmail.com>
2016-11-22 11:09:54 +0530

    Change in iisnode.yml and server.js

--- server.js
+++ server.js
@@ -38,8 +38,8 @@ var allowCrossDomain = function (req, res, next) {
 };
 app.use(allowCrossDomain);
 
-app.get('/*', function (req, res) {console.log('index.html');
-    res.sendFile(path.join(__dirname, 'public','index.html'));
+app.get('/*', function (req, res) {console.log('index1.html');
+    res.sendFile(path.join(__dirname, 'public','index1.html'));
     //res.render(path.join(__dirname, 'public/index1.html'));
 });
 
