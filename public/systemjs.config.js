93b1a8e52aab274760ca593693e3a7c76af14e4d
somnathsaha2015 <somnathsaha2005@gmail.com>
2016-12-19 16:50:15 +0530

    Commit #9 on 12-19-2016

--- public/systemjs.config.js
+++ public/systemjs.config.js
@@ -9,26 +9,22 @@
       // our app is within the app folder
       app: 'app',
       // angular bundles
-      '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
-      '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
-      '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
-      '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
-      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-      '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
-      '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
-      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js'
+      '@angular/core': 'npm:@angular/core/bundles/core.umd.min.js',
+      '@angular/common': 'npm:@angular/common/bundles/common.umd.min.js',
+      '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.min.js',
+      '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.min.js',
+      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.min.js',
+      '@angular/http': 'npm:@angular/http/bundles/http.umd.min.js',
+      '@angular/router': 'npm:@angular/router/bundles/router.umd.min.js',
+      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.min.js'
       // other libraries
       , 'rxjs': 'npm:rxjs'
       , 'angular-in-memory-web-api': 'npm:angular-in-memory-web-api'
-      , 'ng2-modal': 'node_modules/ng2-modal'
-      , "ng2-bootstrap": "npm:ng2-bootstrap/bundles/ng2-bootstrap.umd.js"
+      , 'ng2-modal': 'node_modules/ng2-modal'  
+      , "ng2-bootstrap": "npm:ng2-bootstrap"
       , 'moment': 'npm:moment'
-      // , 'mydatepicker': 'npm:mydatepicker'
       , 'primeng': 'npm:primeng'
-      , '@ng-idle/core':'npm:@ng-idle/core/bundles/core.umd.min.js'
-      //, 'ng2-bootstrap/ng2-bootstrap': 'npm:ng2-bootstrap/bundles/ng2-bootstrap.umd.js'
-      // , 'jquery': 'npm:jquery/dist/jquery.min.js'
-      // , 'bootstrap':'npm:bootstrap/dist/js/bootstrap.min.js'
+      , '@ng-idle/core': 'npm:@ng-idle/core/bundles/core.umd.min.js'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
@@ -45,15 +41,12 @@
       }
       , 'ng2-modal': { "main": "index.js", "defaultExtension": "js" }
       , 'moment': { main: 'moment.js', defaultExtension: 'js' }
-      // , mydatepicker: {
-      //   defaultExtension: 'js'
-      // }
       , primeng: {
         defaultExtension: 'js'
       }
-      // , 'ng2-bootstrap/ng2-bootstrap': {
-      //   defaultExtension: 'js'
-      // }
+      , 'ng2-bootstrap': {
+        defaultExtension: 'js'
+      }
     }
   });
 })(this);
